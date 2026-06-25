e868068fa32de238596d5ddb50152a56116fcb3b
MissMeridian <arietraingirl@gmail.com>
2026-06-25 16:58:39 -0500

    guide fetching, channel info parsing & formatting
    main.py now parses the channels.conf, and process the definitions into an OnCable-friendly format. "num" is interpreted as the channel number and if its a decimal point, the decimal will be used as the subchannel number. Once the channel info has been processed, it fetches the guide according to the endpoint type (only 4broadcast has been implemented so far) and performs the appropriate behavior to fetch the XMLTV for that system. Once it has that, it passes it back to the parse and collect function, which then takes the raw XMLTV string and passes it to xmlproc.py via XMLTV2DEL, which then converts every <programme> tag into an OnCable delimited formatted string line.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os, json
 import logging
+import datetime as DT
+import requests
+import xmlproc as XML
 
 PROGRAM_DIR = os.path.dirname(os.path.abspath(__file__))
 
 log = logging.getLogger('spark')
-
 
 def get_config(name:str):
     '''Attempts to grab a .conf file by root name, and falls back to .example.conf suffix if it fails.'''
@@ -32,8 +34,54 @@ def get_config(name:str):
     log.error(f"No configuration was loaded for '{name}', Spark may not operate correctly (or not at all)!")
     return {} # blank dict
 
+def fetch_guide(endpoint_type:str,endpoint_url:str,date:DT.datetime) -> None|str:
+    '''Fetches XMLTV (as a string) for the appropriate endpoint and the date provided.'''
+    guide = None
+    if endpoint_type == "4broadcast":
+        # collect API response from 4broadcast
+        request_date = date.strftime("%m/%d")
+        request_url = f"{endpoint_url}/guide/{request_date}?xmltv=true"
+        r = requests.get(request_url)
+        r.raise_for_status()
+        guide = r.content.decode() # assumes that we received a proper bytestring of XML response
+    
+    return guide
+
+def parse_and_collect(channels:dict, date:DT.datetime=DT.datetime.now()) -> list:
+    '''Parses the channel definitions (channels) and runs the fetch tasks for each and for the specified date. Returns a list of pipe-separated program lines, in the OnCable format.'''
+    records = []
+    for channel in channels:
+        # process the channel number into the OnCable-friendly numbers
+        channel_number = channel.get("num", None)
+        subchannel = None
+        try:
+            v = str(float(channel_number)).split(".") # HEHEHE LOL THIS IS DIABOLICAL (but, it works...)
+            channel_number = v[0].rjust(3, "0") # (in HU-961 voice) "FIRST DIGIT."
+            subchannel = v[1][:1] # (in HU-961 voice) "SECOND DIGIT." - only one digit on the minor channel
+            if subchannel == "0": subchannel = None # null it out if there's no subchannel
+        except:
+            log.warning(f"Invalid value for 'num' in channel definition: {channel}, this definition will be ignored")
+            continue # skip this bad definition
+        # collect the channel's callsign
+
+        channel_info = {
+            "ch_num": channel_number,
+            "subch": subchannel,
+        }
+        # prepare collection for the proper endpoint types
+        endpoint_url = channel.get("endpoint", None)
+        endpoint_type = channel.get("type", "4broadcast") # assume 4broadcast if unspecified
+        endpoint_target = channel.get("target_id", None) # if unspecified, we'll just grab the first channel in XMLTV
+        guide_xml = fetch_guide(endpoint_type, endpoint_url, date)
+        if guide_xml:
+            XML.XMLTV2DEL(guide_xml, endpoint_target, channel_info)
+            # to-do, process these to a file
+
+
+
 if __name__ == "__main__":
     import coloredlogs
     coloredlogs.install("DEBUG")
-    get_config('channels')
-    get_config('export')
+    channels = get_config('channels')
+    parse_and_collect(channels)
+    
